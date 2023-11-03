5bc60e61355ce2e3fbc2377afe27830a26dc9fcc
MahiHailu <hail.mulat@gmail.com>
2023-11-03 04:44:16 -0700

    new commit

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,113 @@
+#include "binary_trees.h"
+/**
+ * new_node - Function to creates a new_node in a linked_list
+ * @node: Enter the pointer to the node to create
+ * Return: Node created
+ */
+link_t *new_node(binary_tree_t *node)
+{
+	link_t *new;
+
+	new =  malloc(sizeof(link_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+/**
+ * free_q - A function that free a nodes in a linked list
+ * @head: linked_list node
+ */
+void free_q(link_t *head)
+{
+	link_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
+/**
+ * _push - Function to push a node onto the stack
+ * @node: Type pointer of the tree's node
+ * @head: Enter the head node in the stack
+ * @tail: Enter the tail node of on the stack
+ */
+void _push(binary_tree_t *node, link_t *head, link_t **tail)
+{
+	link_t *new;
+
+	new = new_node(node);
+	if (new == NULL)
+	{
+		free_q(head);
+		exit(1);
+	}
+	(*tail)->next = new;
+	*tail = new;
+}
+/**
+ * _pop - Function that pops a node into the stack
+ * @head: Type head node of in the stack
+ */
+void _pop(link_t **head)
+{
+	link_t *temp_node;
+
+	temp_node = (*head)->next;
+	free(*head);
+	*head = temp_node;
+}
+/**
+ * binary_tree_is_complete - Function that checks if a binary tree is complete
+ * @tree: Type pointer of node of the tree
+ * Return: 1 if is complete 0 if it is not
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	link_t *head, *tail;
+	int flag = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	head = tail = new_node((binary_tree_t *)tree);
+	if (head == NULL)
+	{
+		exit(1);
+	}
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->left, head, &tail);
+		}
+		else
+			flag = 1;
+		if (head->node->right != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->right, head, &tail);
+		}
+		else
+			flag = 1;
+		_pop(&head);
+	}
+	return (1);
+}
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * is_full_recursive - Recursively checks if binary tree is full.
+ * @tree: Pointer to the root node of the tree to check.
+ *
+ * Return: 0 if tree is not full,
+ *        1 Otherwise.
+ */
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if ((tree->left == NULL && tree->right != NULL) ||
+			(tree->left != NULL && tree->right == NULL) ||
+			is_full_recursive(tree->left) == 0 ||
+			is_full_recursive(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: A pointer to the root node of the tree to
+ * check.
+ *
+ * Return: 0 if tree is not full,
+ *         1 Otherwise.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_full_recursive(tree));
+}
